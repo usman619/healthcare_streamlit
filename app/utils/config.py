6be7db83eceb447c751e98add2aacc3cf6c1b1e8
usman619 <usmanshahab191@gmail.com>
2025-02-27 18:18:10 +0500

    fix url issue

--- app/utils/config.py
+++ app/utils/config.py
@@ -2,5 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-API_BASE_URL = os.getenv("API_BASE_URL")
+# Streamlit does not support environment variables
+# API_BASE_URL = os.getenv("API_BASE_URL")
+API_BASE_URL = "https://web-production-983ec.up.railway.app/"
