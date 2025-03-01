25ba6fcda6d6c60df5ad7c7a672d1a84f5bbe735
usman619 <usmanshahab191@gmail.com>
2025-03-02 03:05:26 +0500

    updated: base url

--- app/utils/config.py
+++ app/utils/config.py
@@ -4,4 +4,5 @@ import os
 # load_dotenv()
 # Streamlit does not support environment variables
 # API_BASE_URL = os.getenv("API_BASE_URL")
-API_BASE_URL = "https://web-production-983ec.up.railway.app/"
+
+API_BASE_URL = "https://web-production-983ec.up.railway.app"
